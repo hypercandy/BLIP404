5bdb1338a8d92ff4664edb6c2f27d335816b3f17
Amiele Nadine Miguel <anm76@uclive.ac.nz>
2025-04-08 19:32:53 +1200

    delete runner scripts

--- runner-scripts/staging.sh
+++
@@ -1,6 +0,0 @@
-fuser -k 9500/tcp || true
-
-java -jar staging/libs/home-helper-0.0.1-SNAPSHOT.jar \
-    --server.port=9500 \
-    --server.servlet.contextPath=/test \
-    --spring.application.name=home-helper
